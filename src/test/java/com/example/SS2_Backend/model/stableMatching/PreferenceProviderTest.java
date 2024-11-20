3da1332569eed322b57c584d554f084666b51208
Khanh Duy <duykhanh471@protonmail.com>
2024-11-20 18:41:36 +0700

    Thử xây dựng Test (Prototype, chưa hoàn thành Implementation)

---
+++ src/test/java/com/example/SS2_Backend/model/stableMatching/PreferenceProviderTest.java
@@ -0,0 +1,10 @@
+package com.example.SS2_Backend.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PreferenceProviderTest {
+
+}
---
+++ src/test/java/com/example/SS2_Backend/model/stableMatching/SMTProblemUnitTest.java
@@ -0,0 +1,20 @@
+package com.example.SS2_Backend.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SMTProblemUnitTest {
+    // @Test
+    // void testWithSampleGeneratedData() {
+    //     SampleDataGenerator.main(new String[0]);
+    // }
+
+//   looks how to test
+//    Employee employee1 = new Employee("1", 1000);
+//    Employee employee2 = new Employee("2", 2000);
+//		employees.addAll(asList(employee1, employee2));
+//    assertThat(employeeRepository.findAll())
+//            .containsExactly(employee1, employee2);
+}
